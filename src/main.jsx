417ffa23120886e0f4449e98936855282f9605f1
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-08-16 10:47:27 -0500

    feat: Añadir proveedor de tema y variables CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
+import { ThemeProvider } from "@emotion/react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import theme from "./theme/index.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
-    </StrictMode>,
+        <ThemeProvider theme={theme}>
+            <CssVarsProvider theme={theme}>
+                <App />
+            </CssVarsProvider>
+        </ThemeProvider>
+    </StrictMode>
 );
