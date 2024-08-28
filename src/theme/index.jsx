980a957f9b01c311a40546502dc6338e791a06b3
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-08-28 17:25:43 -0500

    feat: Agregar soporte para lazy loading en campo de imágenes

--- src/theme/index.jsx
+++ src/theme/index.jsx
@@ -1,5 +1,22 @@
-import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+import {
+    createTheme,
+    experimental_extendTheme as extendTheme,
+} from "@mui/material/styles";
 
-const theme = extendTheme();
+import { esES as coreEsES } from "@mui/material/locale";
+import { esES as pickersEsES } from "@mui/x-date-pickers/locales";
 
-export default theme;
+const customTheme = createTheme(
+    {
+        palette: {
+            primary: { main: "#1976d2" },
+        },
+    },
+    pickersEsES,
+    coreEsES,
+);
+
+const extendedTheme = extendTheme(customTheme);
+
+export default customTheme;
+export { extendedTheme };
