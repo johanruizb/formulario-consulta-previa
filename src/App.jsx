6c337f7b20d14b7c3ffe3759a150113f3b36c009
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-08-28 17:26:08 -0500

    feat: Agregar soporte para lazy loading en campo de imágenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import "./App.css";
 import FullScreenDialog from "./pages/Form";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
     return (
         <Fragment>
             <FullScreenDialog />
+            <SpeedInsights />
         </Fragment>
     );
 }
