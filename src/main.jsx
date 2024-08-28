a20650112d382081826e89f08c967d2d31d75e9e
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-08-28 17:26:12 -0500

    feat: Agregar soporte para lazy loading en campo de imágenes

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,25 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import theme from "./theme/index.jsx";
+import customTheme, { extendedTheme } from "./theme/index.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <ThemeProvider theme={theme}>
-            <CssVarsProvider theme={theme}>
-                <App />
+        <ThemeProvider theme={customTheme}>
+            <CssVarsProvider theme={extendedTheme}>
+                <LocalizationProvider
+                    dateAdapter={AdapterDayjs}
+                    adapterLocale="es"
+                >
+                    <App />
+                </LocalizationProvider>
             </CssVarsProvider>
         </ThemeProvider>
-    </StrictMode>
+    </StrictMode>,
 );
