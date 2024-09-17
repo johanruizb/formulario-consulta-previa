95733fcfc739655d288a3680e45c1563f501304d
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-16 19:37:20 -0500

    Optimizar manejo de imágenes en DocumentField

--- src/components/Form/DocumentImage.jsx
+++ src/components/Form/DocumentImage.jsx
@@ -1,3 +1,4 @@
+import AspectRatio from "@mui/joy/AspectRatio";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
@@ -8,11 +9,13 @@ import PropTypes from "prop-types";
 import { forwardRef, useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { useController, useFormContext } from "react-hook-form";
+import Reverso from "../../assets/reverso.png";
+import Frente from "../../assets/frente.png";
 
 const DocumentField = forwardRef(function DocumentField(props, ref) {
     const low = false;
     const [url, setUrl] = useState();
-    const { getInputProps } = props;
+    const { getInputProps, placeholder } = props;
 
     useEffect(() => {
         if (low && props.image) {
@@ -40,41 +43,59 @@ const DocumentField = forwardRef(function DocumentField(props, ref) {
             ref={ref}
             elevation={0}
             sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
                 width: "100%",
-                height: { xs: low ? 56 : 56 * 3, md: low ? 56 : 56 * 4 },
+                height: "100%",
             }}
         >
             <input {...getInputProps()} />
-            {url ? (
-                low ? (
-                    <Typography textAlign="center" variant="body2">
-                        {url}
-                    </Typography>
+            <AspectRatio variant="plain">
+                {url ? (
+                    low ? (
+                        <Typography textAlign="center" variant="body2">
+                            {url}
+                        </Typography>
+                    ) : (
+                        <Box
+                            sx={{
+                                // border: 1,
+                                // borderColor: "rgba(0, 0, 0, 0.25)",
+                                mt: "6px",
+                                width: "calc(100% - 1px)",
+                                height: "calc(100% - 6px - 1px)",
+                                backgroundImage: `url(${url})`,
+                                backgroundSize: "cover",
+                                backgroundPosition: "center",
+                                display: "flex",
+                            }}
+                        />
+                    )
                 ) : (
-                    <Box
-                        sx={{
-                            // border: 1,
-                            // borderColor: "rgba(0, 0, 0, 0.25)",
-                            mt: "6px",
-                            width: "calc(100% - 1px)",
-                            height: "calc(100% - 6px - 1px)",
-                            backgroundImage: `url(${url})`,
-                            backgroundSize: "cover",
-                            backgroundPosition: "center",
-                            display: "flex",
-                        }}
-                    />
-                )
-            ) : (
-                <Typography textAlign="center" variant="body2">
-                    Se aceptan archivos .png, .jpg, .jpeg
-                    <br />
-                    Peso maximo de 10MB
-                </Typography>
-            )}
+                    <Box>
+                        <img
+                            src={placeholder}
+                            alt="Imagen"
+                            width="100%"
+                            height="100%"
+                        />
+                        <Typography
+                            textAlign="center"
+                            variant="body2"
+                            sx={{
+                                position: "absolute",
+                                right: "50%",
+                                top: "50%",
+                                transform: "translate(50%, -50%)",
+                                bgcolor: "white",
+                                p: "5px",
+                            }}
+                        >
+                            Se aceptan archivos .png, .jpg, .jpeg
+                            <br />
+                            Peso maximo de 10MB
+                        </Typography>
+                    </Box>
+                )}
+            </AspectRatio>
         </Paper>
     );
 });
@@ -84,6 +105,7 @@ DocumentField.propTypes = {
     value: PropTypes.string,
     defaultValue: PropTypes.string,
     image: PropTypes.object,
+    placeholder: PropTypes.string,
 };
 
 function FrontDocumentImage() {
@@ -132,7 +154,11 @@ function FrontDocumentImage() {
             }}
             InputProps={{
                 inputComponent: DocumentField,
-                inputProps: { getInputProps, image: acceptedFiles[0] },
+                inputProps: {
+                    getInputProps,
+                    image: acceptedFiles[0],
+                    placeholder: Frente,
+                },
             }}
             sx={{
                 "*": {
@@ -190,7 +216,11 @@ function BackDocumentImage() {
             }}
             InputProps={{
                 inputComponent: DocumentField,
-                inputProps: { getInputProps, image: acceptedFiles[0] },
+                inputProps: {
+                    getInputProps,
+                    image: acceptedFiles[0],
+                    placeholder: Reverso,
+                },
             }}
             sx={{
                 "*": {
