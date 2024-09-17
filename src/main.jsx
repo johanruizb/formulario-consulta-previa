1cdcc50d6e43e43cd6851f3920d8106c1ad83588
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-16 19:37:36 -0500

    Optimizar manejo de formularios en FullScreenDialog y OtherConnectivity

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
+import {
+    Experimental_CssVarsProvider as CssVarsProvider,
+    THEME_ID as MATERIAL_THEME_ID,
+} from "@mui/material/styles";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { StrictMode } from "react";
@@ -11,16 +14,22 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import customTheme, { extendedTheme } from "./theme/index.jsx";
+import { CssVarsProvider as JoyCssVarsProvider } from "@mui/joy/styles";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <ThemeProvider theme={customTheme}>
+        <ThemeProvider
+            // theme={customTheme}
+            theme={{ [MATERIAL_THEME_ID]: customTheme }}
+        >
             <CssVarsProvider theme={extendedTheme}>
                 <LocalizationProvider
                     dateAdapter={AdapterDayjs}
                     adapterLocale="es"
                 >
-                    <App />
+                    <JoyCssVarsProvider>
+                        <App />
+                    </JoyCssVarsProvider>
                 </LocalizationProvider>
             </CssVarsProvider>
         </ThemeProvider>
