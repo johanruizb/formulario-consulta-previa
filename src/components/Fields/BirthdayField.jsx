6035da59a93bb6abb4238f8f2b9abf09b68dbedd
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-16 19:37:14 -0500

    Optimizar manejo de formularios en BirthdayField

---
+++ src/components/Fields/BirthdayField.jsx
@@ -0,0 +1,119 @@
+import Typography from "@mui/material/Typography";
+import { DatePicker } from "@mui/x-date-pickers";
+import PropTypes from "prop-types";
+import { Controller, useFormContext } from "react-hook-form";
+// import { useRenderCount } from "@uidotdev/usehooks";
+// import { isDevelopment } from "../../utils/isProduction";
+
+import dayjs from "dayjs";
+import "dayjs/locale/es";
+import { useMemo } from "react";
+
+function YearCount({ date }) {
+    const parsed = useMemo(() => {
+        if (dayjs.isDayjs(date)) return date;
+        else if (typeof date === "string") {
+            const formattedDate = dayjs(date);
+            return formattedDate.isValid() ? formattedDate : null;
+        }
+
+        return null;
+    }, [date]);
+
+    return (
+        parsed && (
+            <Typography variant="body2" flex="none">
+                {`${dayjs(dayjs()).diff(parsed, "years")} años`}
+            </Typography>
+        )
+    );
+}
+
+YearCount.propTypes = {
+    date: PropTypes.any,
+};
+
+export default function BirthdayField({ slotProps }) {
+    // const renderCount = useRenderCount();
+
+    const { control } = useFormContext();
+
+    const {
+        controller: controllerProps,
+        field: { InputProps, ...fieldProps },
+    } = slotProps;
+
+    // const devProps = isDevelopment
+    //     ? {
+    //           endAdornment: (
+    //               <Typography variant="caption">{renderCount}</Typography>
+    //           ),
+    //       }
+    //     : {};
+
+    // const fieldPropsWithDev = {
+    //     InputProps: {
+    //         ...devProps,
+    //         ...InputProps,
+    //     },
+    // };
+
+    return (
+        <Controller
+            control={control}
+            render={({ field, fieldState: { error } }) => {
+                return (
+                    <DatePicker
+                        {...field}
+                        onChange={(date) => {
+                            console.log("instance", dayjs.isDayjs(date));
+                            const formattedDate = dayjs(date);
+                            if (formattedDate.isValid()) {
+                                field.onChange(formattedDate);
+                            } else {
+                                field.onChange("");
+                            }
+                        }}
+                        value={
+                            dayjs.isDayjs(field.value)
+                                ? field.value
+                                : typeof field.value === "string"
+                                  ? dayjs(field.value)
+                                  : null
+                        }
+                        // format="DD/M/YYYY"
+                        autoComplete="one-time-code"
+                        slotProps={{
+                            textField: {
+                                name: field.name,
+                                id: field.name,
+                                fullWidth: true,
+                                error: Boolean(error?.type || error?.types),
+                                helperText: error?.message ?? " ",
+                                // ...fieldPropsWithDev,
+                                InputProps: {
+                                    endAdornment: (
+                                        <YearCount date={field.value} />
+                                    ),
+                                    ...InputProps,
+                                },
+                            },
+                            field: {
+                                autoComplete: "one-time-code",
+                            },
+                        }}
+                        {...fieldProps}
+                    />
+                );
+            }}
+            {...controllerProps}
+        />
+    );
+}
+
+BirthdayField.propTypes = {
+    slotProps: PropTypes.shape({
+        controller: PropTypes.object.isRequired,
+        field: PropTypes.object.isRequired,
+    }).isRequired,
+};
