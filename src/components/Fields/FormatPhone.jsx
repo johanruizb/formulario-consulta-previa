966e5450f3cdda902965bef981b88152348879b3
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-24 19:33:26 -0500

    Optimizar manejo de importaciones en FormatPhone.jsx

--- src/components/Fields/FormatPhone.jsx
+++ src/components/Fields/FormatPhone.jsx
@@ -1,14 +1,14 @@
-import { IMaskInput } from "react-imask";
 import PropTypes from "prop-types";
+
 import { forwardRef } from "react";
-// import { COLOMBIA } from "../constant";
+
+import { IMaskInput } from "react-imask";
 
 const PhoneMask = forwardRef(function PhoneMask(props, ref) {
     const { onChange, ...other } = props;
     return (
         <IMaskInput
             {...other}
-            // mask={`+${COLOMBIA.phonecode} #00 000 0000`}
             mask={`#00 000 0000`}
             definitions={{
                 "#": /[1-9]/,
