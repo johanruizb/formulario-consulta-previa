ca858306206c31b946c670dce3bff5b97f297d01
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-09-24 19:33:52 -0500

    Optimizar manejo de importaciones en LargeQuestion.jsx

---
+++ src/components/Form/LargeQuestion.jsx
@@ -0,0 +1,82 @@
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormLabel from "@mui/material/FormLabel";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import { Controller, useFormContext } from "react-hook-form";
+
+import PropTypes from "prop-types";
+
+function LargeQuestion({ slotProps }) {
+    const { control } = useFormContext();
+    const { controller: controllerProps } = slotProps;
+
+    return (
+        <Controller
+            control={control}
+            render={({ field, fieldState: { error } }) => (
+                <FormControl
+                    required
+                    fullWidth
+                    error={Boolean(error?.type || error?.types)}
+                >
+                    <FormLabel
+                        id="demo-form-control-label-placement"
+                        sx={{
+                            color: "black",
+                        }}
+                    >
+                        Le interesa seguir fortaleciendo sus conocimiento y
+                        competencias sobre Consulta Previa a través de un
+                        diplomado virtual gratuito de 120 horas certificado por
+                        la Universidad del Valle.
+                    </FormLabel>
+                    <RadioGroup
+                        row
+                        aria-labelledby="demo-form-control-label-placement"
+                        name="position"
+                        defaultValue="top"
+                        sx={{
+                            display: "flex",
+                            flexDirection: "row",
+                            justifyContent: "center",
+                        }}
+                        value={field.value}
+                        onChange={field.onChange}
+                        onBlur={field.onBlur}
+                    >
+                        <FormControlLabel
+                            value="si"
+                            control={<Radio />}
+                            label="Si"
+                            labelPlacement="bottom"
+                        />
+                        <FormControlLabel
+                            value="no"
+                            control={<Radio />}
+                            label="No"
+                            labelPlacement="bottom"
+                        />
+                    </RadioGroup>
+                    <FormHelperText
+                        sx={{
+                            textAlign: "center",
+                        }}
+                    >
+                        {error?.message ?? " "}
+                    </FormHelperText>
+                </FormControl>
+            )}
+            {...controllerProps}
+        />
+    );
+}
+
+LargeQuestion.propTypes = {
+    slotProps: PropTypes.shape({
+        controller: PropTypes.object.isRequired,
+    }).isRequired,
+};
+
+export default LargeQuestion;
