b498d698c3a7e3727626c4a49592f45e167fbf55
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-10-06 00:15:59 -0500

    Optimizar manejo de importaciones en src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,59 @@
-import { ThemeProvider } from "@emotion/react";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+
+import { CssVarsProvider as JoyCssVarsProvider } from "@mui/joy/styles";
 import {
-    Experimental_CssVarsProvider as CssVarsProvider,
     THEME_ID as MATERIAL_THEME_ID,
+    ThemeProvider,
 } from "@mui/material/styles";
+
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App.jsx";
 import "./index.css";
-import customTheme, { extendedTheme } from "./theme/index.jsx";
-import { CssVarsProvider as JoyCssVarsProvider } from "@mui/joy/styles";
+import customTheme from "./theme/index.jsx";
+import { SWRConfig } from "swr";
+
+const router = createBrowserRouter([
+    {
+        path: "/*",
+        element: <App />,
+    },
+]);
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <ThemeProvider
-            // theme={customTheme}
-            theme={{ [MATERIAL_THEME_ID]: customTheme }}
-        >
-            <CssVarsProvider theme={extendedTheme}>
-                <LocalizationProvider
-                    dateAdapter={AdapterDayjs}
-                    adapterLocale="es"
-                >
-                    <JoyCssVarsProvider>
-                        <App />
-                    </JoyCssVarsProvider>
-                </LocalizationProvider>
-            </CssVarsProvider>
+        <ThemeProvider theme={{ [MATERIAL_THEME_ID]: customTheme }}>
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="es">
+                <JoyCssVarsProvider>
+                    <SWRConfig
+                        value={{
+                            revalidateOnMount: true,
+                            revalidateOnFocus: false,
+                            refreshInterval: 15000,
+                            fetcher: async (...args) => {
+                                const res = await fetch(...args);
+                                return res.ok
+                                    ? res.json()
+                                    : Promise.reject({
+                                          status: res.status,
+                                          statusText: res.statusText,
+                                      });
+                            },
+                        }}
+                    >
+                        <RouterProvider router={router} />
+                    </SWRConfig>
+                </JoyCssVarsProvider>
+            </LocalizationProvider>
         </ThemeProvider>
     </StrictMode>,
 );
