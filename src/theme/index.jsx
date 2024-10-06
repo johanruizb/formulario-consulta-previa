1fd04bcb4e7eb19d93cbd7561e81fa915b0be96c
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-10-06 00:15:30 -0500

    Optimizar manejo de importaciones en src/theme/index.jsx

--- src/theme/index.jsx
+++ src/theme/index.jsx
@@ -1,22 +1,24 @@
-import {
-    createTheme,
-    experimental_extendTheme as extendTheme,
-} from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
 
 import { esES as coreEsES } from "@mui/material/locale";
 import { esES as pickersEsES } from "@mui/x-date-pickers/locales";
 
 const customTheme = createTheme(
     {
-        palette: {
-            primary: { main: "#1976d2" },
-        },
+        // palette: {
+        //     primary: { main: "#1976d2" },
+        // },
+        cssVariables: true,
+        // colorSchemes: {
+        //     light: true,
+        //     // dark: true
+        // },
     },
     pickersEsES,
     coreEsES,
 );
 
-const extendedTheme = extendTheme(customTheme);
+// const extendedTheme = extendTheme(customTheme);
 
 export default customTheme;
-export { extendedTheme };
+// export { extendedTheme };
