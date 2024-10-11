29f95825a3049d00364ae1d442fa54d8b50a81c9
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-10-11 13:07:34 -0500

    Añadir funciones de utilidad

--- src/pages/Form/functions.jsx
+++ src/pages/Form/functions.jsx
@@ -43,3 +43,23 @@ function getButtonsFooter(curso, small) {
 }
 
 export { getBanner, getFooter, getButtonsFooter };
+
+const replaceAllSpaces = (e, { onBlur, onChange }) => {
+    e.target.value = e.target.value?.replaceAll(" ", "") ?? "";
+    onBlur?.(e);
+    onChange?.(e);
+};
+
+const trimSpaces = (e, { onBlur, onChange }) => {
+    e.target.value = e.target.value?.trim() ?? "";
+    onBlur?.(e);
+    onChange?.(e);
+};
+
+const toUpperCase = (e, { onBlur, onChange }) => {
+    e.target.value = e.target.value?.toUpperCase() ?? "";
+    onBlur?.(e);
+    onChange?.(e);
+};
+
+export { replaceAllSpaces, trimSpaces, toUpperCase };
