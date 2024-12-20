59c9b21ed272b3cd311779b1709183c891646979
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2024-12-20 00:52:33 -0500

    Añadir ruta para la página de error 404 en la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import FullScreenDialog from "./pages/Form";
 import Home from "./pages/Home";
+import ErrorNotFound from "./pages/Error/404";
 
 function App() {
     return (
         <Fragment>
             <Routes>
                 <Route path=":curso" element={<FullScreenDialog />} />
+                <Route path="404" element={<ErrorNotFound />} />
                 <Route path="/" element={<Home />} />
             </Routes>
         </Fragment>
