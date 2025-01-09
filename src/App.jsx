e1ce0d16154417b49eb5a5c3d16816fd8c3a757e
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2025-01-09 09:16:33 -0500

    Agregar componente AlertDialog y ajustar importaciones en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
+
+import AlertDialog from "./components/AlertDialog";
+import ErrorNotFound from "./pages/Error/404";
 import FullScreenDialog from "./pages/Form";
 import Home from "./pages/Home";
-import ErrorNotFound from "./pages/Error/404";
 
 function App() {
     return (
@@ -13,6 +16,7 @@ function App() {
                 <Route path="404" element={<ErrorNotFound />} />
                 <Route path="/" element={<Home />} />
             </Routes>
+            <AlertDialog />
         </Fragment>
     );
 }
