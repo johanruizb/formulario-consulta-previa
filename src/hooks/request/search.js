3acba23973f34589239a4239d1cc057b0e98ced8
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2025-01-09 09:15:59 -0500

    Refactor búsqueda de usuarios: cambiar método de obtención de token y actualizar la estructura de la función de búsqueda

--- src/hooks/request/search.js
+++ src/hooks/request/search.js
@@ -2,43 +2,14 @@ import { URI } from "../../components/constant";
 
 const url = URI.API.replace("/v1", "");
 
-const getToken = () => {
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            username: "cristiank",
-            password: "feny8uhjddsj90",
-        }),
-    };
-
-    return fetch(`${url}/token/`, options);
-};
-
-const SEARCH_DOC = {
-    /**
-     * Registra un log enviando los datos al servidor.
-     *
-     * @param {FormData} data - Los datos del log.
-     * @returns {Promise<Response>} - Una promesa que se resuelve con la respuesta del servidor.
-     */
-    existUser: async (num_doc) => {
-        const token = await getToken();
-        const response = await token.json();
-        return fetch(url + `/usuarios/search/inscritos/${num_doc}`, {
-            method: "GET",
+const SEARCH = {
+    verify: ({ documentNumber, turnstile_token }) =>
+        fetch(url + "/usuarios/search/inscritos", {
+            method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                Authorization: `Bearer ${response.access}`,
             },
-        });
-    },
-    token: async () => {
-        const token = await getToken();
-        const response = await token.json();
-        console.log(response);
-    },
+            body: JSON.stringify({ documentNumber, turnstile_token }),
+        }),
 };
-export default SEARCH_DOC;
+export default SEARCH;
