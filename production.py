9e20adc1be389cf1cbcccb60df007288779bea78
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2025-01-21 12:31:58 -0500

    Comentar funciones de construcción y despliegue en production.py

--- production.py
+++ production.py
@@ -1,34 +1,34 @@
-import os
-from datetime import datetime
-import shutil
+# import os
+# from datetime import datetime
+# import shutil
 
 
-root = os.path.dirname(__file__)
-dist = os.path.join(root, "dist")
+# root = os.path.dirname(__file__)
+# dist = os.path.join(root, "dist")
 
-server = os.path.join(root, "..", "consulta-previa-proxy")
-server_dist = os.path.join(server, "dist")
-
-
-def build():
-    os.chdir(root)
-    os.system("npm run format")
-    os.system("npm run build")
+# server = os.path.join(root, "..", "consulta-previa-proxy")
+# server_dist = os.path.join(server, "dist")
 
 
-def comment_server():
-    os.chdir(server_dist)
-    shutil.rmtree(server_dist, ignore_errors=True)
-    shutil.copytree(dist, server_dist, dirs_exist_ok=True, ignore=shutil.ignore_patterns("*.git"))
-    os.system("git add .")
-    os.system('git commit -m "Actualizar archivos estaticos"')
-    # os.system("git push origin master")
+# def build():
+#     os.chdir(root)
+#     os.system("npm run format")
+#     os.system("npm run build")
 
 
-def deploy():
-    build()
-    comment_server()
+# def comment_server():
+#     os.chdir(server_dist)
+#     shutil.rmtree(server_dist, ignore_errors=True)
+#     shutil.copytree(dist, server_dist, dirs_exist_ok=True, ignore=shutil.ignore_patterns("*.git"))
+#     os.system("git add .")
+#     os.system('git commit -m "Actualizar archivos estaticos"')
+#     # os.system("git push origin master")
 
 
-if __name__ == "__main__":
-    deploy()
+# def deploy():
+#     build()
+#     comment_server()
+
+
+# if __name__ == "__main__":
+#     deploy()
