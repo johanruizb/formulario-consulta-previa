2a0bba36225c070eb9ea2566b962e6bdcaa7c43d
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2025-01-31 21:17:33 -0500

    Refactor rutas en App.jsx para simplificar la navegación y agregar ListaEspera como ruta principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,79 @@
 import { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import "./App.css";
-
 import AlertDialog from "./components/AlertDialog";
-import ErrorNotFound from "./pages/Error/404";
-import FullScreenDialog from "./pages/Form";
-import Home from "./pages/Home";
+// import ErrorNotFound from "./pages/Error/404";
+// import FormularioRegistro from "./pages/Form";
+// import Home from "./pages/Home";
+
+import "./App.css";
+import ListaEspera from "./pages/Espera";
+// const Home = lazy(() => import("./pages/Home"));
+// const FormularioRegistro = lazy(() => import("./pages/Form"));
+// const ErrorNotFound = lazy(() => import("./pages/Error/404"));
 
 function App() {
     return (
         <Fragment>
             <Routes>
-                <Route path=":curso" element={<FullScreenDialog />} />
-                <Route path="404" element={<ErrorNotFound />} />
-                <Route path="/" element={<Home />} />
+                <Route path="*" element={<ListaEspera />} />
+                {/* <Route
+                    path=":curso"
+                    element={
+                        <Suspense
+                            fallback={
+                                <Backdrop
+                                    open={true}
+                                    sx={{
+                                        bgcolor: "transparent",
+                                    }}
+                                >
+                                    <CircularProgress />
+                                </Backdrop>
+                            }
+                        >
+                            <FormularioRegistro />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path="404"
+                    element={
+                        <Suspense
+                            fallback={
+                                <Backdrop
+                                    open={true}
+                                    sx={{
+                                        bgcolor: "transparent",
+                                    }}
+                                >
+                                    <CircularProgress />
+                                </Backdrop>
+                            }
+                        >
+                            <ErrorNotFound />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path="/"
+                    element={
+                        <Suspense
+                            fallback={
+                                <Backdrop
+                                    open={true}
+                                    sx={{
+                                        bgcolor: "transparent",
+                                    }}
+                                >
+                                    <CircularProgress />
+                                </Backdrop>
+                            }
+                        >
+                            <Home />
+                        </Suspense>
+                    }
+                /> */}
             </Routes>
             <AlertDialog />
         </Fragment>
